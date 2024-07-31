51a4243feec01d56ba4d17e566ad92f9334ba68c
Muhammad Zulqarnain Awan <97059048+muhammad-zulqarnain-awan@users.noreply.github.com>
2024-07-31 23:40:01 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import{getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX4YJTF8_mPrTqYGq2NGiwxMrlLXiXsw4",
-  authDomain: "codex-f9ef7.firebaseapp.com",
-  projectId: "codex-f9ef7",
-  storageBucket: "codex-f9ef7.appspot.com",
-  messagingSenderId: "1043293964327",
-  appId: "1:1043293964327:web:26c7ac905662f402f52602",
-  measurementId: "G-XDFG02WEZ0"
+  apiKey: "apikey-here",
+  authDomain: "website-auth-domain-here",
+  projectId: "project-id-here",
+  storageBucket: "project-storage-bucket-here",
+  messagingSenderId: "messageing-sender-id-here",
+  appId: "app-id-here",
+  measurementId: "measureemnt-id-here"
 };
 
 // Initialize Firebase
